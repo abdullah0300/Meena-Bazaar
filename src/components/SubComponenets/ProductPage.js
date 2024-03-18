276d70946620495b0bd480e244eb2aff6811b0f6
Hanzla Saadi <hanzalasaadi@gmail.com>
2024-03-18 11:04:50 +0500

    pending: filters in product page

--- src/components/SubComponenets/ProductPage.js
+++ src/components/SubComponenets/ProductPage.js
@@ -13,6 +13,117 @@ import { FaRegSquare } from "react-icons/fa";
 import Navbar from "../shared/Navbar.jsx";
 import { apiUrl } from "../../data/env.js";
 
+function BarFilter({ filter, handleSelectOpt, handleFilterChange }) {
+  const [open1, setOpen1] = useState(false);
+  const [icon1, setIcon1] = useState(<BsChevronUp />);
+  const changeIcon1 = () => {
+    setIcon1(!icon1);
+    setOpen1(!open1);
+  };
+
+  return (
+    <div>
+      <div
+        className="flex justify-center item-center px-2 border-gray-300 rounded-md border mx-2 items-baseline"
+        onClick={changeIcon1}
+        style={{
+          overflowY:
+            filter.options && filter.options.length > 5 ? "scroll" : "auto",
+        }}
+      >
+        <p className="text-[#59A0B8] text-[15px] px-2 pt-2 py-2 text-base font-semibold flex">
+          {filter.alternateName}
+        </p>
+        <p>
+          {icon1 ? (
+            <BsChevronDown className="text-[#707070] font-semibold" />
+          ) : (
+            <BsChevronUp className="text-[#707070] font-semibold" />
+          )}
+        </p>
+      </div>{" "}
+      {open1 ? (
+        <ul
+          className="flex flex-col justify-start item-center px-2 border-gray-300 rounded-md border mx-2 items-baseline"
+          style={{
+            maxHeight:
+              filter.options && filter.options.length > 5 ? "200px" : "auto",
+            overflowY: "auto",
+          }}
+        >
+          {filter.options?.map((opt, i) => {
+            return (
+              <li className="flex justify-start gap-2  item-center" key={i}>
+                <input
+                  type="checkbox"
+                  id={opt}
+                  className="text-[#707070] text-[15px] px-2 py-2 text-base font-semibold flex"
+                  onChange={(e) =>
+                    handleFilterChange(filter._id, opt, e.target.checked)
+                  }
+                />
+                <label htmlFor={opt} className="py-2">
+                  {opt}
+                </label>
+              </li>
+            );
+          })}
+        </ul>
+      ) : null}
+    </div>
+  );
+}
+
+function BarFilterMobile({ filter, handleFilterChange }) {
+  const [open1, setOpen1] = useState(false);
+  const [icon1, setIcon1] = useState(<BsChevronUp />);
+  const changeIcon1 = () => {
+    setIcon1(!icon1);
+    setOpen1(!open1);
+  };
+
+  return (
+    <div>
+      <div
+        className="flex justify-center item-center  px-2 border-gray-300 rounded-md border mx-2 items-baseline "
+        onClick={changeIcon1}
+      >
+        <p className=" text-[#59A0B8] text-[15px] px-2 pt-2 py-2 text-base  font-semibold flex">
+          {filter.alternateName}
+        </p>
+        <p className="">
+          {icon1 ? (
+            <BsChevronDown class="text-[#707070] font-semibold" />
+          ) : (
+            <BsChevronUp class="text-[#707070] font-semibold" />
+          )}
+        </p>
+      </div>
+      {open1 ? (
+        <ul className="flex flex-col justify-start item-center  px-2 border-gray-300 rounded-md border mx-2 items-baseline ">
+          {filter.options?.map((opt, i) => {
+            return (
+              <li class="flex justify-start item-center " key={i}>
+                <input
+                  type="checkbox"
+                  id={opt}
+                  className="text-[#707070] text-[15px] px-2 py-2 text-base font-semibold flex"
+                  onChange={(e) =>
+                    handleFilterChange(filter._id, opt, e.target.checked)
+                  }
+                />
+                <label htmlFor={opt} className="py-2">
+                  {opt}
+                </label>
+              </li>
+            );
+          })}
+        </ul>
+      ) : null}
+    </div>
+  );
+}
+
 function ProductPage({
   products,
   // currentCategory,
@@ -25,8 +136,8 @@ function ProductPage({
   const { categoryId, currentCategoryName } = useParams();
   console.log("hellllll", categoryId);
 
-  const filteredProducts = products?.filter(
-    (prod) => prod.category === categoryId
+  const filteredBarFilters = [...filters].filter(
+    (fil) => fil.categoryId === categoryId
   );
 
   // const filteredCategory = categories?.filter(cat => cat._id === currentCategory);
@@ -135,12 +246,102 @@ function ProductPage({
     ),
   };
 
+  function handleFilterProducts() {
+    const allOptions = document.querySelectorAll(".special-element");
+
+    const optionsArr = allOptions.map((el) =>
+      el.dataset.opt ? el.dataset.opt : null
+    );
+    console.log(optionsArr);
+  }
+
+  const [selectedFilters, setSelectedFilters] = useState({});
+
+  // Filter products based on the selected filters
+  const filteredProducts = products
+    ?.filter((p) => p.category === categoryId)
+    .filter((product) => {
+      return Object.entries(selectedFilters).every(([filterId, options]) => {
+        // If no options are selected for a filter, include the product
+        if (options.length === 0) return true;
+        // Otherwise, check if the product has at least one of the selected options for the filter
+        return options.some((option) =>
+          product.chosenFilters.some(
+            (filter) =>
+              filter.filterId === filterId && filter.chosenOption === option
+          )
+        );
+      });
+    });
+
+  // Update selected filters when a checkbox is changed
+  const handleFilterChange = (filterId, option, checked) => {
+    setSelectedFilters((prevFilters) => {
+      // Clone the previous selected filters object
+      const updatedFilters = { ...prevFilters };
+      // If the filter is not yet in the selected filters, initialize it as an empty array
+      if (!updatedFilters[filterId]) {
+        updatedFilters[filterId] = [];
+      }
+      // Update the selected options for the filter based on the checkbox change
+      if (checked) {
+        updatedFilters[filterId].push(option);
+      } else {
+        updatedFilters[filterId] = updatedFilters[filterId].filter(
+          (selectedOption) => selectedOption !== option
+        );
+      }
+      return updatedFilters;
+    });
+  };
+
   return (
     <div class="mt-0 md:mt-52">
       <Navbar categories={categories} filters={filters} />
       <h2 class="fs-1 py-5 font-bold text-center text-[#BD9229]">
         {currentCategoryName}
       </h2>
+
+      <Container fluid>
+        <Row>
+          <Col xs={4} md={2}>
+            <p class="font-semibold py-3 text-center text-[#59A0B8]">
+              Scroll Left
+            </p>
+          </Col>
+          <Col xs={12} md={9}>
+            {" "}
+            <div class="md:block hidden">
+              <Slider {...settings}>
+                {filteredBarFilters?.map((fil, i) => {
+                  return (
+                    <BarFilter
+                      filter={fil}
+                      handleSelectOpt={handleFilterProducts}
+                      handleFilterChange={handleFilterChange}
+                      key={i}
+                    />
+                  );
+                })}
+              </Slider>
+            </div>
+            <div class="block md:hidden">
+              <Slider {...settings1}>
+                {filteredBarFilters?.map((fil, i) => {
+                  return (
+                    <BarFilterMobile
+                      filter={fil}
+                      handleSelectOpt={handleFilterProducts}
+                      handleFilterChange={handleFilterChange}
+                      key={i}
+                    />
+                  );
+                })}
+              </Slider>
+            </div>
+          </Col>
+        </Row>
+      </Container>
 
       <Container
         fluid
