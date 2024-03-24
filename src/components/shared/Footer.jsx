d1e43a98f7339c79c0fa1c8e6cd095af96999907
Abdullah Aslam <aslamabdullah724@gmail.com>
2024-03-24 21:35:38 +0500

    footer logo setting

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -103,7 +103,19 @@ const Footer = () => {
 
         {/* first div */}
         <div className=' hidden md:flex gap-40 text-white pl-16 pt-10'>
-
+          <div className=' flex flex-col gap-3'>
+            {/* <div>
+              <h3 className='text-lg uppercase font-semibold'>Our resources</h3>
+              <hr className='w-12 my-1 rounded-lg' style={{ border: "3px solid #BD9229" }} />
+            </div>
+            <div className=' flex flex-col gap-3 capitalize text-[#E5E5E5]'>
+              <p>plan and pricing</p>
+              <p>client business</p>
+              <p>sales & purchase</p>
+              <p>product quality</p>
+            </div> */}
+            <img className='h-[10rem] rounded' src='https://ik.imagekit.io/mctozv7td/WhatsApp%20Image%202024-02-28%20at%2002.49.09_5c1c6572.jpg?updatedAt=1709070883137'></img>
+          </div>
           {/* 1st */}
           <div className=' flex flex-col gap-5'>
             <div className='flex flex-col gap-3'>
@@ -158,7 +170,7 @@ const Footer = () => {
               <p>sales & purchase</p>
               <p>product quality</p>
             </div> */}
-            <img className='h-[10rem] rounded-pill' src='https://ik.imagekit.io/mctozv7td/WhatsApp%20Image%202024-02-28%20at%2002.49.09_5c1c6572.jpg?updatedAt=1709070883137'></img>
+            {/* <img className='h-[10rem] rounded-pill' src='https://ik.imagekit.io/mctozv7td/WhatsApp%20Image%202024-02-28%20at%2002.49.09_5c1c6572.jpg?updatedAt=1709070883137'></img> */}
           </div>
 
         </div>
