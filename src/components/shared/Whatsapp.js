0e7627c26b0afa40d969585421e39f03fd66d712
Abdullah Aslam <aslamabdullah724@gmail.com>
2024-03-26 19:28:50 +0500

    whatsapp styling

--- src/components/shared/Whatsapp.js
+++ src/components/shared/Whatsapp.js
@@ -4,7 +4,7 @@ const WhatsAppButton = ({  }) => {
     
   
     return (
-        <div id="whatsapp-button" class="whatsapp-button">
+        <div id="whatsapp-button" class="whatsapp-button ">
         <a href="https://wa.me/+447403113223?text=Hello%20there!" target="_blank">
           <img src="https://ik.imagekit.io/mctozv7td/Vaping%20Circle/whatsapp.png?updatedAt=1711462415230" alt="WhatsApp Icon"/>
         </a>
